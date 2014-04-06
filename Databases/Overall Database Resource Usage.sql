c0fb631edb8edb847e5b3c849b4a45d54af82e66
Mark S. Rasmussen <msr@ipaper.dk>
2014-04-06 18:59:31 +0200

    Update overall DB usage script to include CPU and IO in same

--- Databases/Overall Database Resource Usage.sql
+++ Databases/Overall Database Resource Usage.sql
@@ -1,5 +1,5 @@
 /*
-	Title:         Overall CPU Usage per Database
+	Title:         Overall Database Resource Usage
 	Created by:    Mark S. Rasmussen <mark@improve.dk>
 	License:       CC BY 3.0
 	Attribution:   Inspired by Glenn Berry's DMV scripts
@@ -7,7 +7,7 @@
 	Requirements:  2005+
 
 	Usage:
-	Outputs an overview of how much CPU time each database has spent since the last
+	Outputs an overview of how much/IO resources database has spent since the last
 	DBCC FREEPROCCACHE / restart.
 */
 
@@ -15,17 +15,21 @@ WITH DatabaseUsage AS
 (
 	SELECT
 		DB_Name(PA.DatabaseID) AS [Database Name],
-		SUM(total_worker_time) AS [CPU Time (ms)]
+		SUM(total_worker_time) AS [CPU Time (ms)],
+		(SUM(IO.num_of_bytes_read) + SUM(IO.num_of_bytes_written)) / 1024 / 1024 AS [IO (mb)]
 	FROM
-		sys.dm_exec_query_stats AS qs
+		sys.dm_exec_query_stats AS QS
 	CROSS APPLY
 		(SELECT CAST(value AS int) AS DatabaseID FROM sys.dm_exec_plan_attributes(qs.plan_handle) WHERE attribute = 'dbid') AS PA
+	LEFT JOIN
+		sys.dm_io_virtual_file_stats(NULL, NULL) AS IO ON IO.database_id = PA.DatabaseID
 	GROUP BY
 		DatabaseID
 )
 SELECT
 	*,
-	CAST([CPU Time (ms)] * 1.0 / SUM([CPU Time (ms)]) OVER() * 100.0 AS DECIMAL(5, 2)) AS [CPU Time (%)]
+	CAST([CPU Time (ms)] * 1.0 / SUM([CPU Time (ms)]) OVER() * 100.0 AS DECIMAL(5, 2)) AS [CPU Time (%)],
+	CAST([IO (mb)] * 1.0 / SUM([IO (mb)]) OVER() * 100.0 AS DECIMAL(5, 2)) AS [IO (%)]
 FROM
 	DatabaseUsage
 ORDER BY
