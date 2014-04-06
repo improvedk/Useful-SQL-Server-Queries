953b54be861959586cf28c947e3882ead31982d6
Mark S. Rasmussen <mark@improve.dk>
2014-04-06 14:11:50 +0200

    Built in setting recommendations

--- Server Configuration/Settings Overview.sql
+++ Server Configuration/Settings Overview.sql
@@ -17,7 +17,19 @@ SELECT
 	maximum,
 	description,
 	is_dynamic,
-	is_advanced
+	is_advanced,
+	CASE
+		WHEN name = 'backup compression default' AND value = 0 THEN
+			'It is generally recommended to enable backup compression unless you'' bound by CPU during backups.'
+		WHEN name = 'clr enabled' AND value = 1 THEN
+			'CLR should only be enabled if it''s needed, otherwise it''s better to leave it disabled.'
+		WHEN name = 'max server memory (MB)' AND value = 2147483647 THEN
+			'You should always specify a max value for the ''max server memory'' setting to avoid SQL Server starving the OS and/or other instances.'
+		WHEN name = 'optimize for ad hoc workloads' AND value = 0 THEN
+			'You should generally always enable the ''optimize for adhoc workloads'' setting.'
+		ELSE
+			NULL
+	END AS Recommendation
 FROM
 	sys.configurations
 ORDER BY
