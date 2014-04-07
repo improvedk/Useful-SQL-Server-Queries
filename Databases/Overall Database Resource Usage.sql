5c223006ee61963d590418cab8d3b27e2f9e4267
Mark S. Rasmussen <msr@ipaper.dk>
2014-04-07 08:32:34 +0200

    Now also includes plan cache usage

--- Databases/Overall Database Resource Usage.sql
+++ Databases/Overall Database Resource Usage.sql
@@ -24,8 +24,24 @@ WITH DatabaseUsage AS
 		(SELECT CAST(value AS int) AS DatabaseID FROM sys.dm_exec_plan_attributes(qs.plan_handle) WHERE attribute = 'dbid') AS PA
 	LEFT JOIN
 		sys.dm_io_virtual_file_stats(NULL, NULL) AS IO ON IO.database_id = PA.DatabaseID
+	WHERE
+		PA.DatabaseID < 32767
 	GROUP BY
 		DatabaseID
+),
+PlanUsage AS
+(
+	SELECT
+		PA.DatabaseID,
+		SUM(P.size_in_bytes) / 1024 / 1024 AS [Plan Cache (mb)]
+	FROM
+		sys.dm_exec_cached_plans P
+	CROSS APPLY
+		(SELECT CAST(value AS int) AS DatabaseID FROM sys.dm_exec_plan_attributes(p.plan_handle) WHERE attribute = 'dbid') AS PA
+	WHERE
+		PA.DatabaseID < 32767
+	GROUP BY
+		PA.DatabaseID
 ),
 BPUsage AS
 (
@@ -35,8 +51,7 @@ BPUsage AS
 	FROM
 		sys.dm_os_buffer_descriptors WITH (NOLOCK)
 	WHERE
-		database_id > 4 AND -- system databases
-		database_id <> 32767 -- ResourceDB
+		database_id < 32767
 	GROUP BY
 		database_id
 )
@@ -45,15 +60,19 @@ SELECT
 	CAST([CPU Time (ms)] * 1.0 / SUM([CPU Time (ms)]) OVER() * 100.0 AS DECIMAL(5, 2)) AS [CPU (%)],
 	CAST([IO (mb)] * 1.0 / SUM([IO (mb)]) OVER() * 100.0 AS DECIMAL(5, 2)) AS [IO (%)],
 	CAST([Buffer Pool (mb)] * 1.0 / SUM([Buffer Pool (mb)]) OVER() * 100.0 AS DECIMAL(5, 2)) AS [Buffer Pool (%)],
+	CAST([Plan Cache (mb)] * 1.0 / SUM([Plan Cache (mb)]) OVER() * 100.0 AS DECIMAL(5, 2)) AS [Plan Cache (%)],
 	[CPU Time (ms)],
 	[IO (mb)],
-	[Buffer Pool (mb)]
+	[Buffer Pool (mb)],
+	[Plan Cache (mb)]
 FROM
 	sys.databases D
 LEFT JOIN
 	DatabaseUsage U ON U.DatabaseID = D.database_id
 LEFT JOIN
 	BPUsage BP ON BP.DatabaseID = D.database_id
+LEFT JOIN
+	PlanUsage PU ON PU.DatabaseID = D.database_id
 ORDER BY
 	[CPU Time (ms)] DESC
 OPTION
